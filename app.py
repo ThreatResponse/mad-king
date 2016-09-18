3adb3d988edbe09f57c9be4600477ecc712b1e4d
andrewkrug <andrewkrug@gmail.com>
2016-09-18 10:24:43 -0700

    sts show caller info shows on recon

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template
+import inventory
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/recon')
+def recon():
+    who = inventory.Who()
+    instances = inventory.Instance().get_all_running()
+    return render_template('recon.html', instances=instances, who=who)
 
 # We only need this for local development.
 if __name__ == '__main__':
---
+++ inventory.py
@@ -0,0 +1,67 @@
+import boto3
+
+class Connect(object):
+    def __init__(self, region='us-west-2', service='ec2'):
+        self.region = region
+        self.service = service
+
+        try:
+          self.client = boto3.client(service,region)
+        except:
+          raise StandardError(
+            "No AWS Credentials could be found."
+          )
+
+class Region(object):
+  def __init__(self):
+    self.client = Connect().client
+
+  def get_all(self):
+    availRegions = []
+    regions = self.client.describe_regions()
+    for region in regions['Regions']:
+        availRegions.append(region['RegionName'])
+    return availRegions
+
+class Instance(object):
+  def __init__(self):
+    self.client = Connect().client
+
+  def get_running_by_region(self, region):
+    inventory = []
+
+    reservations = Connect(region).client.describe_instances(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]
+            )['Reservations']
+
+    for reservation in reservations:
+      for instance in reservation['Instances']:
+          instance_data = self.__extract_data(instance)
+          instance_data['region'] = region
+          inventory.append(instance_data)
+
+    return inventory
+
+  def __extract_data(self, instance):
+    return dict(
+      public_ip_address = instance.get('PublicIpAddress', None),
+      instance_id = instance['InstanceId'],
+      launch_time = instance['LaunchTime'],
+      platform = instance.get('Platform', None),
+      vpc_id = instance['VpcId'],
+      ami_id = instance['ImageId'],
+      volume_ids = [ bdm['Ebs']['VolumeId'] for bdm in instance.get('BlockDeviceMappings', [] ) ],
+      )
+
+  def get_all_running(self):
+    inventory = {}
+
+    for region in Region().get_all():
+      inventory[region] = self.get_running_by_region(region)
+
+    return inventory
+
+class Who(object):
+    def __init__(self):
+        self.client = boto3.client('sts')
+        self.identity = self.client.get_caller_identity()
